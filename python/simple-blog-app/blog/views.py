40a90937625dab5cdd20b9ec448a79f10dfa192a
bad-logic <45297269+bad-logic@users.noreply.github.com>
2018-11-29 12:12:32 +0545

    added comment section

--- python/simple-blog-app/blog/views.py
+++ python/simple-blog-app/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models  import Post
+from .models  import Post, Comment
 from django.utils import timezone
-from .forms import PostForm
+from .forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -14,21 +15,23 @@ def post_detail(request, pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request, 'blog/post_detail.html',{'post':post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit = False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail',pk=post.pk)
+
 
 
     else:
         form = PostForm()
         return render(request,'blog/post_edit.html',{'form':form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post,pk=pk)
     if request.method == "POST":
@@ -36,10 +39,55 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit = False)
             post.author = request.user
-            post.published_date = timezone.now()
+            post.published_date = None#setting published_date None so it moves to draft
             post.save()
             return redirect('post_detail',pk=post.pk)
 
     else:
         form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html',{'form':form})
+
+@login_required
+def post_draft_list(request):
+    drafts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request,'blog/post_draft_list.html',{'drafts':drafts})
+
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish()# no need to save because it is saved in publish() method see blog/models.py
+    return redirect('post_detail',pk=post.pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+@login_required
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = CommentForm()
+        return render(request,'blog/add_comment_to_post.html',{'form':form})
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)

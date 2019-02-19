8cb8e90d7fc72738cdc2d97e515d4f6f5edd0906
Roshan Bhattarai <45297269+bad-logic@users.noreply.github.com>
2019-02-19 20:24:38 +0545

    Update settings.py

--- python/blog/blog_sample/settings.py
+++ python/blog/blog_sample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=%jsrfit%hq@$if@k_u=+%az1)^3%2zc6*8fcv)^o*d$*=)k#4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition

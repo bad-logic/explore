04a4b6e4cf0ec04cbc3104eec7a567321060df1b
Roshan bhattarai <45297269+bad-logic@users.noreply.github.com>
2020-01-27 10:32:51 +0545

    connecting frontend react to backend node server

--- javascript/simple-post-app/backend_/controller/feed.controller.js
+++ javascript/simple-post-app/backend_/controller/feed.controller.js
@@ -1,8 +1,16 @@
 exports.getPosts = (req, res, next) => {
 
     res.status(200).json({
-        title: 'new post',
-        body: 'sdlkjfl ldsajflkd sldf nice post'
+        posts: [{
+            _id: '1',
+            title: 'First post',
+            content: 'This is a very first post',
+            imageUrl: 'images/mask.jpeg',
+            creator: {
+                name: 'Roshan',
+            },
+            createdAt: new Date()
+        }]
     });
 
 }

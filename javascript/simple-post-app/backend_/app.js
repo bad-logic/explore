32df16bbe27ca9722d7c1c2b7434b30b28f05056
Roshan bhattarai <45297269+bad-logic@users.noreply.github.com>
2020-01-27 10:17:07 +0545

    solving the cors error

--- javascript/simple-post-app/backend_/app.js
+++ javascript/simple-post-app/backend_/app.js
@@ -2,10 +2,18 @@ const { express, parser } = require('./config');
 const app = express();
 const feedRoutes = require('./routes/feed.routes');
 
-// setting the headers for cors error
+// SETTING THE HEADERS FOR CORS ERROR
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
-//  parsing the incoming json format data
+//  PARSING THE INCOMING JSON DATA
 app.use(parser.json());
 
+// ROUTES
+app.use('/feed', feedRoutes);
 
 module.exports = { app };

9d3f7d171a32806985bb9a535ff39594340709d6
roshan <45297269+bad-logic@users.noreply.github.com>
2021-12-14 21:39:21 +0545

    register nodes to the chain network

--- block-chain-demo/src/blockchain.py
+++ block-chain-demo/src/blockchain.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 from time import time
+from urllib.parse import urlparse
 
 
 class Blockchain(object):
@@ -8,9 +9,19 @@ class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.nodes = set()
 
         # create genesis block
         self.new_block(previous_hash=1, proof=100)
+
+    def register_node(self, address: str) -> None:
+        """
+        Add a new node to the list of nodes
+        :param address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
+        :return: None
+        """
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
 
     def new_block(self, proof: int, previous_hash: str = None) -> dict:
         """

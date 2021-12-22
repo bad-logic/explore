3ae068e092da64bc7d0d567a2a333304313cd993
roshan <45297269+bad-logic@users.noreply.github.com>
2021-12-22 06:54:59 +0545

    golang

---
+++ block-chain-demo/golang/go.mod
@@ -0,0 +1,8 @@
+module github.com/bad-logic/block-chain-basic-demo/golang
+
+go 1.13
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/gorilla/mux v1.8.0
+)
---
+++ block-chain-demo/golang/src/server.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
+)
+
+var Blockchain []Block
+
+type Block struct {
+	Index     int
+	Timestamp string
+	BPM       int
+	PrevHash  string
+	Hash      string
+}
+
+func generateHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
+func generateBlock(oldBlock Block, BPM int) (Block, error) {
+	var newBlock Block
+	t := time.Now()
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = t.String()
+	newBlock.BPM = BPM
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = generateHash(newBlock)
+	return newBlock, nil
+}
+
+func isBlockValid(oldBlock, newBlock Block) bool {
+	if oldBlock.Index+1 != newBlock.Index {
+		return false
+	}
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+	if newBlock.Hash != generateHash(newBlock) {
+		return false
+	}
+	return true
+}
+
+func replaceChain(newBlocks []Block) {
+	if len(newBlocks) > len(Blockchain) {
+		Blockchain = newBlocks
+	}
+}
+
+func makeMuxRouter() http.Handler {
+	muxRouter := mux.NewRouter()
+	muxRouter.HandleFunc("/", handleGetBlockchain).Methods("GET")
+	muxRouter.HandleFunc("/", handleWriteBlock).Methods("POST")
+	return muxRouter
+}
+
+func respondWithJSON(request *http.Request, response http.ResponseWriter, statusCode int, payload interface{}) {
+	result, err := json.MarshalIndent(payload, "", " ")
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte("HTTP 500: Internal Server Error"))
+		return
+	}
+	response.WriteHeader(statusCode)
+	response.Write(result)
+}
+
+func handleGetBlockchain(response http.ResponseWriter, request *http.Request) {
+	bytes, err := json.MarshalIndent(Blockchain, "", " ")
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	respondWithJSON(request, response, http.StatusOK, string(bytes))
+}
+
+type Message struct {
+	BPM int
+}
+
+func handleWriteBlock(response http.ResponseWriter, request *http.Request) {
+	var m Message
+	decoder := json.NewDecoder(request.Body)
+	if err := decoder.Decode(&m); err != nil {
+		respondWithJSON(request, response, http.StatusBadRequest, request.Body)
+		return
+	}
+
+	defer request.Body.Close()
+	oldBlock := Blockchain[len(Blockchain)-1]
+	newBlock, err := generateBlock(oldBlock, m.BPM)
+	if err != nil {
+		respondWithJSON(request, response, http.StatusInternalServerError, m)
+		return
+	}
+	if isBlockValid(oldBlock, newBlock) {
+		newBlockchain := append(Blockchain, newBlock)
+		replaceChain(newBlockchain)
+		// spew.Dump is a convenient function that pretty prints our structs into the console. It’s useful for debugging
+		spew.Dump(Blockchain)
+	}
+	respondWithJSON(request, response, http.StatusCreated, newBlock)
+
+}
+
+func runServer() error {
+	handler := makeMuxRouter()
+	httpAddr := os.Getenv("ADDR")
+	log.Println("Listening on ", os.Getenv(("ADDR")))
+	s := &http.Server{
+		Addr:           ":" + httpAddr,
+		Handler:        handler,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+
+	environment := os.Environ()
+
+	log.Println(environment)
+
+	go func() {
+		t := time.Now()
+		genesisBlock := Block{0, t.String(), 0, "", ""}
+		spew.Dump(genesisBlock)
+		Blockchain = append(Blockchain, genesisBlock)
+	}()
+	log.Fatal((runServer()))
+}

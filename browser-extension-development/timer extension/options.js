696f17517f0ce2cb2dc28a88c6044d31d2d19134
Roshan Bhattarai <45297269+bad-logic@users.noreply.github.com>
2023-06-13 17:11:25 +0545

    add storage permission

--- browser-extension-development/timer extension/options.js
+++ browser-extension-development/timer extension/options.js
@@ -1,7 +1,17 @@
 const nameInput = document.getElementById('name-input');
 
 const saveBtn = document.getElementById('save-btn');
+chrome.storage.sync.get(['name'], (res) => {
+  nameInput.value = res.name ?? '';
+});
 
 saveBtn.addEventListener('click', () => {
-  console.log(nameInput.value);
+  chrome.storage.sync.set(
+    {
+      name: nameInput.value,
+    },
+    () => {
+      console.log(`name set to ${nameInput.value}`);
+    }
+  );
 });
--- browser-extension-development/timer extension/popup.js
+++ browser-extension-development/timer extension/popup.js
@@ -1,4 +1,10 @@
 const timeElement = document.getElementById('time');
+const nameElement = document.getElementById('name');
+
+chrome.storage.sync.get(['name'], (res) => {
+  const name = res.name ?? 'user';
+  nameElement.textContent = `Hello, ${name}`;
+});
 
 const currentTime = new Date().toLocaleTimeString();
 

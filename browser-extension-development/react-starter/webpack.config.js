bec0dd49c15f74b31c768c6f119f0339430c2ab2
Roshan Bhattarai <45297269+bad-logic@users.noreply.github.com>
2023-06-27 22:33:38 +0545

    feat: react starter for chrome extension development

---
+++ browser-extension-development/react-starter/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('node:path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'cheap-module-source-map',
+  entry: {
+    popup: path.resolve('src/popup/popup.tsx'),
+    options: path.resolve('src/options/options.tsx'),
+  },
+  module: {
+    rules: [
+      {
+        use: 'ts-loader',
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+      },
+      {
+        type: 'asset/resource',
+        test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve('src/assets'),
+          to: path.resolve('dist'),
+        },
+      ],
+    }),
+    new HtmlPlugin({
+      title: 'React Extension',
+      filename: 'popup.html',
+      chunks: ['popup'],
+    }),
+    new HtmlPlugin({
+      title: 'React Extension Options',
+      filename: 'options.html',
+      chunks: ['options'],
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve('dist'),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+};

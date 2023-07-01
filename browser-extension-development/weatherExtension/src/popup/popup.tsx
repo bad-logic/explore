da015dfea3375b436cd073700a58d8e43d6348bb
Roshan Bhattarai <45297269+bad-logic@users.noreply.github.com>
2023-07-01 23:01:49 +0545

    feat: add weather card

--- browser-extension-development/weatherExtension/src/popup/popup.tsx
+++ browser-extension-development/weatherExtension/src/popup/popup.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
-import WeatherCard from './weatherCard/';
+import WeatherCard from './weatherCard';
 
+// import 'fontsource-roboto';
 import './popup.css';
 
 const App: React.FC<{}> = () => {
   return (
     <div>
       <WeatherCard city="toronto" />
-      <WeatherCard city="toronto" />
-      <WeatherCard city="toronto" />
+      <WeatherCard city="new york" />
+      <WeatherCard city="xiangxao" />
     </div>
   );
 };
--- browser-extension-development/weatherExtension/src/popup/weatherCard/weatherCard.tsx
+++ browser-extension-development/weatherExtension/src/popup/weatherCard/weatherCard.tsx
@@ -3,32 +3,51 @@ import { Box, Card, CardContent, Typography } from '@mui/material';
 
 import { IOpenWeatherData, fetchOpenWeatherData } from '../../utils/api';
 
-const WeatherCard: React.FC<{ city: string }> = ({ city }) => {
-  const [weather, setWeather] = React.useState<IOpenWeatherData | null>(null);
-  React.useEffect(() => {
-    fetchOpenWeatherData('toronto')
-      .then((res) => {
-        setWeather(res);
-      })
-      .catch((err) => {
-        console.error({ err });
-      });
-  }, [city]);
-
-  if (!weather) {
-    return <div>Loading...</div>;
-  }
-
+const WeatherCardContainer: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
   return (
     <Box mx="4px" my="26px">
       <Card>
-        <CardContent>
-          <Typography variant="h5">{weather.name}</Typography>
-          <Typography variant="body1">{Math.round(weather.main.temp)}</Typography>
-          <Typography variant="body1">Feels like: {Math.round(weather.main.feels_like)}</Typography>
-        </CardContent>
+        <CardContent>{children}</CardContent>
       </Card>
     </Box>
+  );
+};
+
+type WeatherCardState = 'loading' | 'error' | 'ready';
+
+const WeatherCard: React.FC<{ city: string }> = ({ city }) => {
+  const [cardState, setCardState] = React.useState<WeatherCardState>('loading');
+  const [weather, setWeather] = React.useState<IOpenWeatherData | null>(null);
+  React.useEffect(() => {
+    fetchOpenWeatherData(city)
+      .then((res) => {
+        setWeather(res);
+        setCardState('ready');
+      })
+      .catch((err) => {
+        console.error({ err });
+        setCardState('error');
+      });
+  }, [city]);
+
+  if (cardState === 'loading' || cardState === 'error') {
+    return (
+      <WeatherCardContainer>
+        <Typography variant="body1">
+          {cardState === 'loading' ? 'Loading...' : 'Error: could not retrieve data for this city.'}
+        </Typography>
+      </WeatherCardContainer>
+    );
+  }
+
+  return (
+    <WeatherCardContainer>
+      <Typography variant="h5">{weather.name}</Typography>
+      <Typography variant="body1">{Math.round(weather.main.temp)}</Typography>
+      <Typography variant="body1">Feels like: {Math.round(weather.main.feels_like)}</Typography>
+    </WeatherCardContainer>
   );
 };
 

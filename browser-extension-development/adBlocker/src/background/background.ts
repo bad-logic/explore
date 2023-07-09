5a5c020a2e11583ee37ef3338b6424a130c2c08f
roshan <45297269+bad-logic@users.noreply.github.com>
2023-07-09 11:10:39 +0545

    feat: using manifest version 3 and rules file

--- browser-extension-development/adBlocker/src/background/background.ts
+++ browser-extension-development/adBlocker/src/background/background.ts
@@ -1,22 +1,22 @@
-// ad urls list https://easylist.to/
+// // ad urls list https://easylist.to/
 
-chrome.webRequest.onBeforeRequest.addListener(
-  (details) => {
-    console.log({ details });
-    // const adUrls = [`*://*.googleadservices.com/*`, `*://*.tpc.googlesyndication.com/*`, '*://*.g.doubleclick.net/*'];
-    const filters = ['googleadservices', 'tpc.googlesyndication', 'g.doubleclick'];
-    let cancel = false;
-    for (const filter of filters) {
-      if (details.url.indexOf(filter) != -1) {
-        cancel = true;
-      }
-    }
-    return {
-      cancel,
-    };
-  },
-  {
-    urls: ['<all_urls>'],
-  },
-  ['blocking'] // this request must be completed before it goes through. blocks request from going though in parallel
-);
+// chrome.webRequest.onBeforeRequest.addListener(
+//   (details) => {
+//     console.log({ details });
+//     // const adUrls = [`*://*.googleadservices.com/*`, `*://*.tpc.googlesyndication.com/*`, '*://*.g.doubleclick.net/*'];
+//     const filters = ['googleadservices', 'tpc.googlesyndication', 'g.doubleclick'];
+//     let cancel = false;
+//     for (const filter of filters) {
+//       if (details.url.indexOf(filter) != -1) {
+//         cancel = true;
+//       }
+//     }
+//     return {
+//       cancel,
+//     };
+//   },
+//   {
+//     urls: ['<all_urls>'],
+//   },
+//   ['blocking'] // this request must be completed before it goes through. blocks request from going though in parallel
+// );

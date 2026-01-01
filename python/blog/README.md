e02328c9c3d617a01afd9e7816be17f3e8cc2c33
roshan <45297269+bad-logic@users.noreply.github.com>
2026-01-01 15:40:08 -0600

    chore: cleanup

--- python/blog/README.md
+++
@@ -1,5 +0,0 @@
-# Simple Blog Application Using Django
-#### Python
-#### CRUD
-#### Django
-
--- python/blog/account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-        path('accounts/login',views.login,name='login'),
-        path('accounts/signup',views.signup ,name='signup'),
-        path('accounts/logout',views.logout,name='logout'),
-]
